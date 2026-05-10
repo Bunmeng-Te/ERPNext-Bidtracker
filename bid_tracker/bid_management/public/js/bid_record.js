326bb8f5a419c5ad44406e90b52d6450fd45600d
Bunmeng Te <tebunmeng2004@gmail.com>
2026-05-11 08:49:35 +1000

    Add automatic Bid Record P&L refresh on form reload

--- bid_tracker/bid_management/public/js/bid_record.js
+++ bid_tracker/bid_management/public/js/bid_record.js
@@ -1,13 +1,29 @@
 frappe.ui.form.on("Bid Record", {
   refresh(frm) {
     if (!frm.is_new()) {
+      // Auto refresh totals from backend
+      frappe.call({
+        method: "bid_tracker.bid_management.api.recalculate_bid",
+        args: {
+          bid_name: frm.doc.name,
+        },
+        callback: async function (r) {
+          if (r.message) {
+            await frm.reload_doc();
+          }
+        },
+      });
+
+      // Manual button
       frm.add_custom_button(__("Recalculate P&L"), async () => {
         const r = await frappe.call({
-          method: "bid_management.api.recalculate_bid",
+          method: "bid_tracker.bid_management.api.recalculate_bid",
           args: { bid_name: frm.doc.name },
         });
+
         if (r.message) {
           await frm.reload_doc();
+
           frappe.show_alert({
             message: __("P&L recalculated"),
             indicator: "green",
