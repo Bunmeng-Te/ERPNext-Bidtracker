e6a717af03523993f3362a5a2d43663c51ed8f2d
Bunmeng Te <tebunmeng2004@gmail.com>
2026-05-11 09:12:58 +1000

    Remove obsolete Bid Cost Entry naming workaround

--- bid_tracker/install.py
+++ bid_tracker/install.py
@@ -1,12 +1,10 @@
 import frappe
 from frappe.custom.doctype.custom_field.custom_field import create_custom_fields
-from frappe.custom.doctype.property_setter.property_setter import make_property_setter
 
 
 def after_install():
     create_timesheet_bid_field()
     create_employee_hourly_rate_field()
-    set_bid_cost_entry_naming()
 
 
 def create_timesheet_bid_field():
@@ -41,30 +39,3 @@ def create_employee_hourly_rate_field():
     }
 
     create_custom_fields(custom_fields, update=True)
-
-
-def set_bid_cost_entry_naming():
-    try:
-        make_property_setter(
-            "Bid Cost Entry",
-            None,
-            "autoname",
-            "format",
-            "Data",
-            validate_fields_for_doctype=False,
-        )
-
-        make_property_setter(
-            "Bid Cost Entry",
-            None,
-            "naming_series",
-            "BCE-.YYYY.-.#####",
-            "Data",
-            validate_fields_for_doctype=False,
-        )
-
-    except Exception:
-        frappe.log_error(
-            frappe.get_traceback(),
-            "Bid Tracker install warning"
-        )
