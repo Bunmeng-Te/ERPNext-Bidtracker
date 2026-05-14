898d129ef14553f1337dbe5a643fc2a6f2ed3362
Bunmeng Te <tebunmeng2004@gmail.com>
2026-05-15 08:48:55 +1000

    Initialize financial fields on Bid Record form refresh

--- bid_tracker/bid_management/doctype/bid_record/bid_record.js
+++ bid_tracker/bid_management/doctype/bid_record/bid_record.js
@@ -1,8 +1,20 @@
 frappe.ui.form.on("Bid Record", {
+  refresh(frm) {
+    initialize_financial_fields(frm);
+  },
+
   estimated_contract_value(frm) {
     calculate_metrics(frm);
   },
 });
+
+function initialize_financial_fields(frm) {
+  frm.set_value("total_timesheet_cost", frm.doc.total_timesheet_cost || 0);
+  frm.set_value("total_manual_cost", frm.doc.total_manual_cost || 0);
+  frm.set_value("total_bid_cost", frm.doc.total_bid_cost || 0);
+  frm.set_value("estimated_profit", frm.doc.estimated_profit || 0);
+  frm.set_value("roi_ratio", frm.doc.roi_ratio || 0);
+}
 
 function calculate_metrics(frm) {
   let contract = frm.doc.estimated_contract_value || 0;
